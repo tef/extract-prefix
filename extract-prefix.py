ac694fa95b3885bf4b7227624c3379b32d76daed
tef <tef@twentygototen.org>
2025-06-12 17:08:09 +0100

    wip: walk from tail commits to head

--- extract-prefix.py
+++ extract-prefix.py
@@ -1,5 +1,7 @@
-import pygit2
+import collections
 import os
+
+import pygit2
 
 repo_dir = os.getcwd()
 
@@ -16,15 +18,22 @@ print("Head", head)
 
 commits = {}
 children = {}
+parents = {}
 
-search = [head]
+search = collections.deque([head])
 walked = set(search)
+tails = set()
 
 while search:
-    idx = search.pop(0)
+    idx = search.popleft()
 
     c = repo.get(idx)
     commits[idx] = c
+    if idx not in children:
+        children[idx] = set()
+
+    if not c.parent_ids:
+        tails.add(idx)
 
     for p in c.parent_ids:
         if p not in children:
@@ -35,9 +44,21 @@ while search:
             search.append(p)
             walked.add(p)
 
+    parents[idx] = list(c.parent_ids)
+
 
 print("commits", len(commits))
 
+search = collections.deque(tails)
+counts = {k:len(v) for k,v in parents.items()}
 
+while search:
+    idx = search.popleft()
+    
+    print("commit", idx)
 
+    for c in children[idx]:
+        counts[c] -= 1
+        if counts[c] == 0:
+            search.append(c)
 
